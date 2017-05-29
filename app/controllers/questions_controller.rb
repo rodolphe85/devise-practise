942fcbc0680382fc0028449f35b7f301bee6d6fc
Rodolphe Geant <rodolphe@rgeant.name>
2017-05-29 10:59:52 +0400

    Add Devise gem and have a running Question model

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
