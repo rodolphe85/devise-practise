9fd383de2e9dedbd4b97989a47b96190dacf0303
Rodolphe Geant <rodolphe@rgeant.name>
2017-06-05 08:22:13 +0400

    Finalise first mouture

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -102,7 +102,7 @@ SimpleForm.setup do |config|
   # config.item_wrapper_class = nil
 
   # How the label text should be generated altogether with the required text.
-   config.label_text = lambda { |label| "#{label}" }
+  # config.label_text = lambda { |label| "#{label}" }
 
   # You can define the class to use on all labels. Default is nil.
   # config.label_class = nil
